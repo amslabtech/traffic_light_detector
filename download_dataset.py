ef9b6027c7200df809ddb3646fc97d4bab8ea87a
YoshikiMaekawa2000 <yoshiki.maekawa@icloud.com>
2023-07-03 15:24:33 +0900

    download script

--- download_dataset.py
+++ download_dataset.py
@@ -4,23 +4,17 @@ import urllib.request
 import zipfile
 
 
-data_dir = os.path.join(os.getcwd(), "./dataset/")
+data_dir = os.path.join(os.getcwd(), "dataset")
+print(data_dir)
 if not os.path.exists(data_dir):
     os.mkdir(data_dir)
-    url = 'https://www.dropbox.com/sh/3moys2qgtgzm8ct/AACtyi8hkM3eennV6LnN1aD0a/vidvipo_full_2023_05_27.zip?dl=0'
-    urllib.request.urlretrieve(url, data_dir)
-    with zipfile.ZipFile(os.path.join(data_dir, 'vidvipo_full_2023_05_27.zip?dl=0'), 'r') as zip_ref:
+    url = 'https://www.dropbox.com/sh/3moys2qgtgzm8ct/AACtyi8hkM3eennV6LnN1aD0a/vidvipo_full_2023_05_27.zip?dl=1'
+    print("DOWNLOADIN ZIP")
+    save_path = os.path.join(data_dir, 'dataset.zip')
+    urllib.request.urlretrieve(url, save_path)
+    print("UNZIP")
+    with zipfile.ZipFile(save_path, 'r') as zip_ref:
         zip_ref.extractall(data_dir)
+    os.remove(save_path)
+    print("SUCSESSFURRY DOWNLOADED")
 
-
-'''
-script_dir=$(cd $(dirname $0); pwd)
-
-
-cd ../.. 
-mkdir dataset && cd dataset
-
-
-wget https://www.dropbox.com/sh/3moys2qgtgzm8ct/AACtyi8hkM3eennV6LnN1aD0a/vidvipo_full_2023_05_27.zip?dl=0
-unzip 'vidvipo_full_2023_05_27.zip?dl=0'
-'''
