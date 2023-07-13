420b1ed6f6294c333ac71577c97a8fd524050386
YoshikiMaekawa2000 <yoshiki.maekawa@icloud.com>
2023-07-13 14:35:28 +0900

    first commit

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='traffic_light_detector',
-    version='1.0.0',
-    packages=find_packages(),
-  
-)
---
+++ src/traffic_light_detector.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import rospy
+from sensor_msgs.msg import CompressedImage, Image
+from cv_bridge import CvBridge
+from ultralytics import YOLO
+
+class TrafficlightDetector:
+    def __init__(self):
+        self.node_name="traffic_light_detector"
+        rospy.init_node(self.node_name)
+
+        # self.model=YOLO("yolov8n.pt")
+        self.model=YOLO("/home/amsl/weights/trafficc_light/vidvip_yolov8n_2023-05-19.pt")
+
+        image_sub = rospy.Subscriber('/CompressedImage', CompressedImage, self.image_callback)
+        self.pub = rospy.Publisher('/yolo_result', Image, queue_size=10)
+
+
+
+
+    def image_callback(self, msg):
+        # 画像データをROSメッセージから復元
+        bridge = CvBridge()
+        image = bridge.compressed_imgmsg_to_cv2(msg)
+
+        # YOLOで推論
+        infer_result = self.model(image)
+
+
+
+        # 推論結果をImage型に変換
+        result_msg = bridge.cv2_to_imgmsg(infer_result[0].plot(), encoding="passthrough")
+
+
+        # 推論結果をpublish
+        self.pub.publish(result_msg)
+
+if __name__=="__main__":
+    TrafficlightDetector()
+    rospy.spin()
