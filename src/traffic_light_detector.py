4aee745fa023b0eb2c872a561e1dd63278eb5526
YoshikiMaekawa2000 <yoshiki.maekawa@icloud.com>
2023-07-14 11:17:32 +0900

    visualize

--- src/traffic_light_detector.py
+++ src/traffic_light_detector.py
@@ -12,10 +12,12 @@ class TrafficlightDetector:
 
         # self.model=YOLO("yolov8n.pt")
         pwd=os.getcwd()
+
+
         self.model=YOLO(os.path.join(pwd[:-3], "weights", "vidvip_yolov8n_2023-05-19.pt"))
 
-        # image_sub = rospy.Subscriber('/CompressedImage', CompressedImage, self.image_callback)
-        image_sub = rospy.Subscriber('/grass_cam/image_raw/compressed', CompressedImage, self.image_callback)
+        image_sub = rospy.Subscriber('/CompressedImage', CompressedImage, self.image_callback)
+        # image_sub = rospy.Subscriber('/grass_cam/image_raw/compressed', CompressedImage, self.image_callback)
         self.pub = rospy.Publisher('/yolo_result', Image, queue_size=10)
 
 
@@ -27,7 +29,15 @@ class TrafficlightDetector:
         image = bridge.compressed_imgmsg_to_cv2(msg)
 
         # YOLOで推論
-        infer_result = self.model(image)
+        infer_result = self.model(image, classes=[15, 16], conf=0.25)
+
+        for box in infer_result[0].boxes:
+            if(int(box.cls.item()) == 15):
+                print("\033[31m#########################\n########SIGNAL RED#######\n#########################\033[0m")
+            elif(int(box.cls.item()) == 16):
+                print("\033[32m###########################\n########SIGNAL BLUE########\n###########################\033[0m")
+
+
 
 
 
