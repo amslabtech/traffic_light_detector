d02d137fe2c1b26a6579d0812e1d46cdf0c5d95e
YoshikiMaekawa2000 <yoshiki.maekawa@icloud.com>
2023-09-21 15:23:53 +0900

    fix launch

--- docker_run.sh
+++ docker_run.sh
@@ -2,8 +2,11 @@ image_name="traffic_light_detector"
 tag_name="noetic"
 
 docker run -it \
+    --ipc=host \
     --network=host \
+    --rm \
     --gpus all \
     --name "traffic_light_detector" \
     $image_name:$tag_name \
-    bash
+    bash \
+    --login
