d78c929e917d87d64e78f7ba4c89f11f84974523
YoshikiMaekawa2000 <yoshiki.maekawa@icloud.com>
2023-09-21 16:31:19 +0900

    docker de launch

---
+++ docker/build_and_exec.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+image_name='traffic_light_detector'
+image_tag='noetic'
+
+# docker build -t $image_name:$image_tag .
+
+./docker_build.sh
+
+docker run -idt \
+    --ipc=host \
+    --network=host \
+    --rm \
+    --gpus all \
+    --name "traffic_light_detector" \
+    $image_name:$image_tag \
+    bash \
+    --login
+
+docker exec traffic_light_detector /home/catkin_ws/src/traffic_light_detector/launch/launch.sh
+
+
---
+++ launch/launch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+roslaunch traffic_light_detector.launch
