7a26e8691400c762e7c79a6914a4b5d256088fca
YoshikiMaekawa2000 <yoshiki.maekawa@icloud.com>
2023-09-22 16:37:38 +0900

    run and launch

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -3,5 +3,5 @@
 image_name='traffic_light_detector'
 image_tag='noetic'
 
-# docker build -t $image_name:$image_tag --no-cache=true .
-docker build -t $image_name:$image_tag .
+docker build -t $image_name:$image_tag --no-cache=true .
+# docker build -t $image_name:$image_tag .
