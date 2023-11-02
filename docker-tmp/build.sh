6d6e33896bd619fd64ae9b907226b12d13602da3
YoshikiMaekawa2000 <yoshiki.maekawa@icloud.com>
2023-11-03 06:55:39 +0900

    Add docker for cuda

--- docker-tmp/build.sh
+++ docker-tmp/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 image_name='traffic_light_detector'
-image_tag='emergency'
+image_tag='tmp'
 
 # docker build -t $image_name:$image_tag --no-cache --network host .
 docker build -t $image_name:$image_tag .
