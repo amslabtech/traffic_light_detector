36b3b0f6171d9e1b092439d1171edfaa25deb66a
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-07-20 14:07:01 +0900

    Remove files

--- docker/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-image_name='traffic_light_detector'
-image_tag='emergency'
-
-# docker build -t $image_name:$image_tag --no-cache --network host .
-docker build -t $image_name:$image_tag .
--- launch/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /opt/ros/noetic/setup.bash
-source /home/amsl/catkin_ws/devel/setup.bash
-export ROS_WORKSPACE=/home/amsl/catkin_ws
-export ROS_PACKAGE_PATH=/home/amsl/catkin_ws/src:$ROS_PACKAGE_PATH
-
-roslaunch traffic_light_detector.launch
